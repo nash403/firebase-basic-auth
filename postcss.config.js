9add8d3622b610b7998921f277c5ed33ea77eadc
Honoré Nintunze <nintu.lab@gmail.com>
2020-05-13 00:05:38 +0200

    🔧 add tailwind to project

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const IN_PRODUCTION = process.env.NODE_ENV === 'production'
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [ `./public/**/*.html`, `./src/**/*.vue`, `./src/**/*.jsx` ],
+  defaultExtractor (content) {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
+  whitelist        : [],
+  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/ ],
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...IN_PRODUCTION ? [purgecss] : [],
+  ],
+}
