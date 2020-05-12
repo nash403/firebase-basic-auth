1d0c422078d1d194c8eee4063aa262fd2c119f2e
Honoré Nintunze <nintu.lab@gmail.com>
2020-05-12 19:42:03 +0200

    🐛 change mobile detection way

--- src/useLogin.ts
+++ src/useLogin.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app'
 import { reactive, toRefs } from '@vue/composition-api'
-import { isStandaloneMode } from '@/util/functions'
+import { isDesktop } from '@/util/functions'
 
 type LoginState = {
   credentials: firebase.auth.UserCredential | null;
@@ -24,19 +24,14 @@ export function useLogin () {
     provider.addScope('https://www.googleapis.com/auth/userinfo.profile')
     provider.addScope('https://www.googleapis.com/auth/userinfo.email')
     const auth = firebase.auth()
-    if (isStandaloneMode()) {
-      auth.signInWithRedirect(provider)
-    } else {
-      auth
-        .signInWithPopup(provider)
-        .then(result => {
-          state.credentials = result
-        })
-        .catch(error => {
-          state.error = error
-          console.error(error)
-        })
+    if (isDesktop()) {
+      auth.signInWithPopup(provider)
+        .then(result => { state.credentials = result })
+        .catch(error => { state.error = error })
         .finally(() => (state.signingIn = false))
+    } else {
+      auth.signInWithRedirect(provider)
+      .catch(error => { state.error = error })
     }
   }
 
--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,2 +1,2 @@
 
-export const isStandaloneMode = () => window.matchMedia('(display-mode: standalone)').matches
+export const isDesktop = () => window.matchMedia('(hover: hover)').matches
